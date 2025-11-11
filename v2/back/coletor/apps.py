f1015b657df607ff115e99c6d90b4df5b2202b67
MathRick05 <matheuslopes0501@hotmail.com>
2025-11-11 13:30:19 -0300

    fix/backend reestruturado

--- v2/back/coletor/apps.py
+++ v2/back/coletor/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ColetasConfig(AppConfig):
+class ColetorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coletas'
+    name = 'coletor'
